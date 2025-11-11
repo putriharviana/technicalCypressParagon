779f0aab1eb431470e81ff67aa0a4556d814e17e
putriharviana <putriharviana@gmail.com>
2025-11-11 20:32:11 +0700

    Initial commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -27,8 +27,6 @@ describe('login scneario', () => {
     cy.get('input[name="password"]').type('Password1234!')
     cy.get('#page-login__button-login').click()
 
-    cy.get('#chakra-modal--body-6 .css-acwcvw').should('have.text', 'Kamu perlu mengisi dan memilih alamat penerima untuk melihat stok produk.')
-
     cy.get('body').then(($body) => {
     if ($body.find('a:contains("Isi Alamat")').length > 0) {
       // Element exists
@@ -36,9 +34,11 @@ describe('login scneario', () => {
         .should('be.visible')
         .then(() => {
           // Do something if visible
+        cy.get('#chakra-modal--body-6 .css-acwcvw').should('have.text', 'Kamu perlu mengisi dan memilih alamat penerima untuk melihat stok produk.')
+        
         cy.contains('a', 'Isi Alamat').click()
         
-        cy.get('.styles_modal-body__container-list__gPpUE').should('exist').scrollTo('bottom') // scrolls to bottom
+        cy.get('.styles_modal-body__container-list__gPpUE', { ensureScrollable: false }).should('exist').scrollTo('bottom') // scrolls to bottom
 
         cy.wait(2000)
         cy.contains('button', 'Alamat Baru').click({ force: true })
@@ -59,9 +59,47 @@ describe('login scneario', () => {
         })
       } else {
         // Element does NOT exist
-        cy.get('.styles_modal-body__container-list__gPpUE', { ensureScrollable: false }).scrollTo('bottom');
-        cy.get('.chakra-button').eq(1).should('be.visible').click()
+          cy.contains('h2', 'Box Puspa').scrollIntoView().should('be.visible')
+          
+          cy.contains('h2', 'Box Puspa').parents('div')
+          .filter((index, el) => Cypress.$(el).find('button').length > 0).first().within(() => {
+            cy.contains('button', 'Keranjang').click()
+          });
+          cy.wait(2000)
+
+          cy.get('.icon-nav ul li').eq(2).should('exist').click()
+
+          cy.contains('button', 'Beli sekarang').click()
+          cy.wait(3000)
       }
+    })
+
+    cy.get('body').then(($body) => {
+        if ($body.find('p:contains("Step 1.")').length > 0) {
+          cy.contains('p', 'Step 1.').should('be.visible').then(() => {
+            // Element exists
+            cy.contains('button', 'Selanjutnya').click()
+            cy.wait(2000)
+            cy.contains('p', 'Step 2.').should('be.visible')
+            cy.contains('button', 'Selanjutnya').click()
+            cy.wait(2000)
+            cy.contains('p', 'Step 3.').should('be.visible')
+            cy.contains('button', 'Selesai').click()
+            cy.wait(2000)
+          })
+        }else {
+          cy.contains('h1', 'Pilih layanan pengiriman').click()
+          cy.wait(2000)
+          cy.contains('p', 'Regular').click()
+          cy.wait(2000)
+          cy.contains('p', 'jne').click()
+          cy.wait(2000)
+          cy.contains('p', 'Masukkan kode atau pilih voucher').click()
+          cy.wait(2000)
+          cy.get('input[placeholder="Masukkan kode promo"]').type('CQT-RS-1234{enter}')
+          cy.wait(2000)
+          cy.contains('button', 'Cari').click()
+        }
     })
   })
 })
